018a74b2fcbc6f88e1f7d8c05470c77bed157460
sanjo johny <119657806+Sanjojohny@users.noreply.github.com>
2026-01-07 21:30:13 +0530

    Update Ledger.cs

--- week2/Day2/Assignment/Ledger.cs
+++ week2/Day2/Assignment/Ledger.cs
@@ -1,8 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 namespace DigitalPettyCashLe;
 
-public class Ledger
+public class Ledger<T> where T : Transacton
 {
+    private List<T> transactions = new List<T>();
 
+    
+    public void AddEntry(T entry)
+    {
+        transactions.Add(entry);
+    }
+
+    
+    public decimal CalculateTotal()
+    {
+        decimal total = 0;
+
+        foreach (T tot in transactions)
+        {
+            total += tot.Amount;
+        }
+
+        return total;
+    }
+
+    
+    public List<T> GetAllTransactions()
+    {
+        return transactions;
+    }
+
+    
+    public List<T> GetTransactionsByDate(DateTime date)
+    {
+        List<T> result = new List<T>();
+
+        foreach (T tot in transactions)
+        {
+            if (tot.Date.Date == date.Date)
+            {
+                result.Add(tot);
+            }
+        }
+
+        return result;
+    }
 }
