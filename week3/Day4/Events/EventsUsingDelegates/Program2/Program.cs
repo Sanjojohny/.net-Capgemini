19cbe3eddd48f611ecfd65fabf8669188e605b75
sanjo johny <119657806+Sanjojohny@users.noreply.github.com>
2026-01-19 10:40:08 +0530

    Add files via upload

---
+++ week3/Day4/Events/EventsUsingDelegates/Program2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Example1;
+class Program
+{
+    public static void Main()
+    {
+        Door obj=new Door();
+        obj.DoorOperations(true);
+        obj.DoorOperations(false);
+    }
+}
